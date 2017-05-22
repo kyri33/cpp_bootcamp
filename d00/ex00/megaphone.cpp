80031025460bb088c21955d70f840ccba2496e0d
Kyri IOULIANOU <kioulian@e5r7p9.jnb.42.fr>
2017-05-22 10:28:31 +0200

    Ex00

---
+++ d00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+void    print_upper(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            std::cout << (char)(str[i] - 'a' + 'A');
+        else
+            std::cout << str[i];
+        i++;
+    }
+}
+
+int     main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEEDBACK NOISE *\n";
+        return (1);
+    }
+    for (int i = 1; i < argc; i++)
+        print_upper(argv[i]);
+    std::cout << "\n";
+}
